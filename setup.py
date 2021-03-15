c3463b35386eec11cc30badf13ac9f74e1116dcb
jacob6838 <51723791+jacob6838@users.noreply.github.com>
2021-03-15 09:29:44 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 from setuptools import setup, find_packages
 import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
-
 setup(
-    name='shared_code',  # Required
-    version='2.0.0',  # Required
-    packages=find_packages(where=''),  # Required
-    python_requires='>=3.6, <4',
+    name="shared_code",  # Required
+    version="2.0.0",  # Required
+    packages=find_packages(where=""),  # Required
+    python_requires=">=3.6, <4",
 )
